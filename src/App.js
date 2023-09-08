20f17a7d107f424075f9dd43e194ab211a8368b6
RenTan <tan.re60@gmail.com>
2023-09-09 07:23:01 +0800

    Second Edit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Sample Upload GitHub</header>
-      <p>First Commit</p>
+      <header className="App-header">
+        <h1>Sample GitHub Upload</h1>
+        <p>First Comment</p>
+      </header>
     </div>
   );
 }
